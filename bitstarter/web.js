4e313c233ad29dc1bd61d32fbb7a3ae665f238da
Gopal Rander <gopalmaheshwari07@gmail.com>
2013-07-21 08:46:58 +0000

    Go

--- bitstarter/web.js
+++ bitstarter/web.js
@@ -3,10 +3,10 @@ var app = express();
 app.use(express.logger());
 
 var buff = fs.readFileSync("index.html");
-buffer.toString();
+buff.toString();
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+  response.send(buff);
 });
 
 var port = process.env.PORT || 5000;
